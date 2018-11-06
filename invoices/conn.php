5b7501de86d932fac43b999443f3f8c63e637ac2
Faizan <ahmedani_faizan@hotmail.com>
2018-11-07 02:34:50 +0500

    deploying

--- invoices/conn.php
+++ invoices/conn.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect("localhost","root","","crud");
+$conn = mysqli_connect("localhost","faizan1","faizan1","crud");
  //$conn = mysqli_connect("85.10.205.173:3306","uzair118","uzair118","uzairdb");
 if (!$conn) {
 	die("Connection failed: " . mysqli_connect_error());
--- shop/db.php
+++ shop/db.php
@@ -1,7 +1,7 @@
 <?php
 // Enter your Host, username, password, database below.
 // I left password empty because i do not set password on localhost.
-$con = mysqli_connect("localhost", "root", "", "crud");
+$con = mysqli_connect("localhost", "faizan1", "faizan1", "crud");
 // Check connection
 if (mysqli_connect_errno())
   {
